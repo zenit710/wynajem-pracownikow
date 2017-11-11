df73f70126b5d7e6d83d44cca9117b6a0500bc0c
Kamil Małek <zenit710@gmail.com>
2017-11-11 23:38:05 +0100

    inicjalizacja projektu

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+
+gulp.task("sass", function () {
+    return gulp
+        .src("./src/style/style.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer({
+            browsers: ["last 2 versions"],
+            cascade: false
+        }))
+        .pipe(gulp.dest("dist/css/internal"));
+});
+
+gulp.task("minify-css", ["sass"], function () {
+    return gulp
+        .src("./dist/css/internal/style.css")
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist/css/internal"));
+});
+
+gulp.task("js", function () {
+    return gulp
+        .src("./src/js/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ["env"]
+        }))
+        .pipe(concat('scripts.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./dist/js/internal"));
+});
+
+gulp.task("minify-js", ["js"], function () {
+    return gulp
+        .src("./dist/js/internal/scripts.js")
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./dist/js/internal"));
+});
+
+gulp.task("build", ["minify-css"]);
+
+gulp.task("watch", function () {
+    gulp.watch("./src/style/**/*.scss", ["sass"]);
+    gulp.watch("./src/js/**/*.js", ["js"]);
+});
