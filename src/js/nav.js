5f1c3517d85310b48148aef776c38966b3228969
Kamil Małek <zenit710@gmail.com>
2017-11-13 23:01:20 +0100

    dodano krotka sekcje o nas, akcja menu na scroll

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,4 +1,6 @@
 let Navigation = function (navbar) {
+    this.NAV_SCROLL_LIMIT = 50;
+
     this.navbar = navbar;
     this.barsButton = navbar.getElementsByClassName('mobile-bars')[0];
     this.linkSection = navbar.getElementsByClassName('links')[0];
@@ -9,14 +11,21 @@ let Navigation = function (navbar) {
 
 Navigation.prototype.init = function () {
     if (!this.initialized) {
-        let bindBarButtonEvents = () => {
+        let bindBarButtonClick = () => {
             this.barsButton.addEventListener('click', () => {
                 this.linkSection.classList.toggle('show');
                 this.barsButton.classList.toggle('active');
             });
         };
 
-        bindBarButtonEvents();
+        let bindMenuScrollClass = () => {
+            document.addEventListener('scroll', () => {
+                this.navbar.classList.toggle('scrolled', window.pageYOffset > this.NAV_SCROLL_LIMIT)
+            });
+        };
+
+        bindBarButtonClick();
+        bindMenuScrollClass();
 
         this.initialized = true;
     }
