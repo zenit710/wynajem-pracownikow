9e9175eac71c0b832e1dc12bc0c03f1a22512c92
Kamil Małek <zenit710@gmail.com>
2017-11-15 23:54:01 +0100

    zmiana organizacji pracownikow

--- src/js/core.js
+++
@@ -1,8 +0,0 @@
-var mySwiper = new Swiper ('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-});
