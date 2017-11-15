7f6e8ef79ab2cd55f3499168c52f8871634b0e54
Kamil Małek <zenit710@gmail.com>
2017-11-15 22:22:06 +0100

    poprawione o nas, dodana ofeta do ostylowania

---
+++ src/js/core.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
